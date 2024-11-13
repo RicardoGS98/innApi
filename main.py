6c657ef1337fc497228ff72fddd2b3cc621dcd88
RicardoGS98 <rgsaez98@gmail.com>
2024-11-13 11:47:15 +0000

    chat a traves de la api (de nuevo)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import base64
 import json
 import os
+import random
 from datetime import datetime, timezone
 
 import requests
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Body
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
 load_dotenv(override=True)
@@ -49,12 +50,107 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
         )
 
 
-# Endpoint protegido
-@app.get("/token")
-async def get_token(payload: dict = Depends(verify_token)):
-    response = requests.post(TOKEN_URL, data=PAYLOAD, headers={'Content-Type': 'application/x-www-form-urlencoded'})
+# Función para obtener un nuevo token
+def get_access_token():
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+
+    response = requests.post(TOKEN_URL, data=PAYLOAD, headers=headers)
 
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Error obtaining access token")
 
     return response.json()
+
+
+# Endpoint protegido
+@app.get("/token")
+async def get_token(payload: dict = Depends(verify_token)):
+    return get_access_token()
+
+
+# Endpoint en FastAPI que maneja la lógica
+@app.post("/chat")
+def request_data(
+        message: str = Body(),
+        conversationId: int = Body(None),
+        companyId: int = Body(None)
+):
+    try:
+        if not conversationId:
+            conversationId = random.randint(1, 9999)
+
+        # 1. Obtener el token
+        access_token = get_access_token()
+
+        # 2. Usar el token para hacer la petición a la API externa
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'id-coversation': str(conversationId),
+            'x-company': str(companyId)
+        }
+        external_response = requests.post(
+            'https://mojito360-bed5bfgee5g4cthk.northeurope-01.azurewebsites.net/api/user-question/',
+            data={'input_question': message},
+            headers=headers
+        )
+
+        # 3. Si la respuesta de la API externa no es 200, devolver un error
+        if external_response.status_code != 200:
+            raise HTTPException(status_code=external_response.status_code, detail="Error fetching external data")
+
+        # 4. Retornar la respuesta de la API externa como la respuesta del endpoint
+        return {**external_response.json(), 'conversationId': conversationId}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.delete("/chat/{conversationId}")
+def delete_chat(
+        conversationId: int,
+):
+    try:
+        # 1. Obtener el token
+        access_token = get_access_token()
+
+        # 2. Usar el token para hacer la petición a la API externa
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Conversation-Id': str(conversationId)
+        }
+        external_response = requests.delete(
+            'https://mojito360-bed5bfgee5g4cthk.northeurope-01.azurewebsites.net/api/clean-conversation/',
+            headers=headers
+        )
+        return external_response.json()
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.post("/bug")
+def post_bug(
+        companyId: int = Body(),
+        conversationId: int = Body(),
+        comment: str = Body()
+):
+    try:
+
+        # 1. Obtener el token
+        access_token = get_access_token()
+
+        # 2. Usar el token para hacer la petición a la API externa
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Conversation-Id': str(conversationId),
+            'Company-Id': str(companyId),
+            'Comment': comment
+        }
+        external_response = requests.post(
+            'https://mojito360-bed5bfgee5g4cthk.northeurope-01.azurewebsites.net/api/bug-report/',
+            headers=headers
+        )
+        return external_response.json()
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
